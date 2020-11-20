bd0e770e16d13bdd12ee6b73e8bbcf108f9bd938
B177679-2020 <Not_provided>
2020-11-20 00:07:58 +0000

    txt file w 250 most similar sequences

--- Assignment2.py
+++ Assignment2.py
@@ -2,13 +2,25 @@
 
 ##FIRST STEP: GET ALL THE SEQUENCES
 
+#Import all modules that the program needs to run
 import os, sys
 import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-'''
+
+#Make sure we are running in the user workspace of the server
+chdir = ("cd /localdisk/home/$USER")
+subprocess.check_output(chdir, shell=True)
+#Create a new directory to store the results
+Directory = input("Type in a name for the directory that will be created in your workspace with the results:")
+os.mkdir(Directory)
+os.chdir(Directory)
+#Create source directory to keep results tidy
+os.mkdir("src")
+os.chdir("src")
+
 #Example query to get the birds G6P protein sequences in fasta format
 #ESearchBirds = 'esearch -db protein -query "txid8782[Organism] AND glucose-6-phosphatase[Protein]" | efetch -db protein -format fasta > birds_G6P.fasta'
 
@@ -30,7 +42,8 @@ num = len([1 for line in UserFile_Contents if line.startswith(">")])
 print("The number of protein sequences for your query is {}".format(num))
 
 ##MULTIPLE SEQUENCE ALIGNMENT WITH CLUSTALO
-Clustalo = 'clustalo -i {}.fasta -o {}MA.fasta -v'.format(TaxonID, TaxonID)
+print("ClustalO multiple alignment is being performed. Please be a little bit patient...")
+Clustalo = 'clustalo -i {}.fasta -o {}MA.fasta -v --output-order tree-order'.format(TaxonID, TaxonID)
 ClustaloOut = subprocess.check_output(Clustalo, shell=True)
 
 #Check the ClustalO output
@@ -42,9 +55,15 @@ print("The number of aligned sequences with clustal is {}".format(numClustal))
 
 #Get consensus sequence
 if numClustal > 250:
+	print("There were more than 250 sequences. BLAST analysis will be performed to keep only the 250 most similar ones")
 	Cons = 'cons -sequence {}MA.fasta -outseq {}Cons.fasta'.format(TaxonID, TaxonID)
 	ConsOut = subprocess.check_output(Cons, shell=True)
-	BlastDB = 'makeblastdb -in {}.fasta -dbtype prot -out {}'.format(TaxonID, TaxonID)
+	print("A consensus sequence was generated")
+	BlastDB = 'makeblastdb -in {}.fasta -dbtype prot'.format(TaxonID)
+	BlastP = 'blastp -query {}Cons.fasta -num_alignments 250 -db {}.fasta -outfmt 7 -out blastout.txt'.format(TaxonID, TaxonID)
+	BlastDBOut = subprocess.check_output(BlastDB, shell=True)
+	BlastPOut = subprocess.check_output(BlastP, shell=True)
+	print("BLAST analysis performed")
 
 #Try with blast
 #blastp -query txid8782Cons.fasta -num_alignments 250 -db txid8782.fasta -outfmt 7 -out blastout.txt
@@ -53,23 +72,49 @@ if numClustal > 250:
 #sort -k3,3nr blastout.txt
 
 
-"""
-##Sort using pandas
-df = pd.read_csv('/localdisk/home/s2100021/Assignment2/try2.txt',sep='\t', skiprows=(0,1,2,3,4))
+#Sort BLAST results by similarity using Pandas
+df = pd.read_csv('blastout.txt',sep='\t', skiprows=(0,1,2,3,4))
+print(df)
 df.columns = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+pd.concat([pd.DataFrame(df.columns), df], ignore_index=True)
 dfsorted = df.sort_values('3', ascending = False)
-AccNumbs = str(dfsorted.iloc[1:250, 1])
+print("Sorted by similarity")
+print(dfsorted)
+#This gets the accession number of the 250 more highly related proteins
 
-UserFile = open('txid8782.fasta')
-UserFile_Contents = UserFile.read()
-print(UserFile_Contents)
+#Try to get accession numbers as a list
+listID = df['2'].to_list()
+ListID250 = listID[1:250]
+print("Acc numbers of the most similar sequences are shown")
+#AccIDs = AccNumbs.split(" ")
+print(ListID250)
 
 
-seqs250=[]
-for AccNumb in AccNumbs:
-	for line in UserFile_Contents:
-		if AccNumb in line:
-			seqs250.append(line)
+UserFile = open('txid8782.fasta', 'r')
+
+FinalFASTA = open("Trial5.txt", mode="w")
+AI_DICT = {}
+for line in ListID250:
+    AI_DICT[line[:-1]] = 1
+
+skip = 0
+for line in UserFile:
+        if line[0] == '>':
+                _splitline = line.split(' ')
+                AccNumbArrow = _splitline[0]
+                accessorID = AccNumbArrow[1:-1]
+                print(accessorID)
+                if accessorID in AI_DICT:
+                        FinalFASTA.write(line)
+                        skip = 0
+                else:
+                        skip = 1
+        else:
+                if not skip:
+                        FinalFASTA.write(line)
+
+UserFile.close()
+FinalFASTA.close()
 
 
 
